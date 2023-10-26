4fa28e9f52f2b1c9f663e706c8157e41bcd8b06b
Zexuan <lzx213@sina.com>
2023-10-25 18:27:19 -0700

    End With First Partner

--- src/main.ts
+++ src/main.ts
@@ -45,17 +45,11 @@ class TileStrorage {
 }
 
 //creating the tilemap nested array
-let tilemap: HTMLImageElement[][] = new Array(numTiles);
+let tilemap: string[][] = Array.from({ length: numTiles }, () =>
+  Array.from({ length: numTiles }, () => "/tile1.png")
+);
 
 const tileStorage = new TileStrorage();
-
-for (let i = 0; i < numTiles; i++) {
-  let row = new Array(numTiles);
-  for (let j = 0; j < numTiles; j++) {
-    row[j] = tileStorage.getTile("/tile1.png");
-  }
-  tilemap[i] = row;
-}
 
 //track the selected tile
 let currentTile = "/tile1.png";
@@ -85,14 +79,12 @@ function drawTexture(
   row: number,
   col: number,
   ctx: CanvasRenderingContext2D,
-  tile: HTMLImageElement,
+  url: string,
   width: number,
   height: number,
   cellSize: number
 ) {
-  tile.onload = () => {
-    ctx.drawImage(tile, row * cellSize, col * cellSize, width, height);
-  };
+  const tile = tileStorage.getTile(url);
   ctx.drawImage(tile, row * cellSize, col * cellSize, width, height);
 }
 
@@ -118,8 +110,7 @@ function redrawTilemap() {
 gridCanvas.addEventListener("click", (e) => {
   const coordX = Math.trunc(e.offsetX / tileSize);
   const coordY = Math.trunc(e.offsetY / tileSize);
-
-  tilemap[coordX][coordY].src = currentTile;
+  tilemap[coordX][coordY] = currentTile;
   redrawTilemap();
 });
 
@@ -128,18 +119,43 @@ gridCanvas.addEventListener("click", (e) => {
 // Loop through the selectable tiles and draw textures in each cell
 function drawSelectCanvas() {
   for (let i = 0; i < numSelectables; i++) {
-    const selectableImage = tileStorage.getTile(imageUrls[i]);
     drawTexture(
       0,
       i,
       selectCtx,
-      selectableImage,
+      imageUrls[i],
       selectCanvas.width,
       selectHeight,
       64
     );
   }
 }
+
+// selectCanvas.addEventListener("click", (e) => {
+//   const coordY = Math.trunc(e.offsetY / selectHeight);
+//   currentTile = imageUrls[coordY];
+// });
+
+let isDraw = false;
+
+gridCanvas.addEventListener("mousedown", () => {
+  isDraw = true;
+});
+
+gridCanvas.addEventListener("mousemove", (e) => {
+  if (isDraw) {
+    const coordX = Math.trunc(e.offsetX / tileSize);
+    const coordY = Math.trunc(e.offsetY / tileSize);
+    tilemap[coordX][coordY] = currentTile;
+    redrawTilemap();
+  }
+});
+
+gridCanvas.addEventListener("mouseup", () => {
+  isDraw = false;
+});
+
+// ... 
 
 selectCanvas.addEventListener("click", (e) => {
   const coordY = Math.trunc(e.offsetY / selectHeight);
